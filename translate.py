56c50cc9a766b06e106dbf0d41dfe5bfa2507283
Byron Smith <bsmith89@gmail.com>
2015-04-23 09:03:27 -0400

    Use argparse.

--- translate.py
+++ translate.py
@@ -1,15 +1,52 @@
 #!/usr/bin/env python3
-"""Translate nucleotide sequences to amino-acids.
-
-"""
+"""Translate nucleotide sequences to amino acids."""
 
 from Bio.SeqIO import parse, write
 import sys
+import argparse
+from copy import copy
+
+DEFAULT_INFILE_FMT = 'fasta'
+DEFAULT_OUTFILE_FMT = 'fasta'
+
+def translate_recs(records):
+    """Translate sequence from records.
+
+    This geneator takes a iterable of Bio.SeqRecord objects and the same
+    records with sequences translated from nucleotides to amino acids.
+
+    """
+    for rec in records:
+        # TODO: Make sure that this shallow copy of rec
+        # is safe.  Is rec.seq a mutable sequence?
+        # No, right?
+        rec = copy(rec)
+        rec.seq = rec.seq.translate()
+        yield rec
 
 def main():
-    for rec in parse(sys.stdin, 'fasta'):
-        rec.seq = rec.seq.translate()
-        write(rec, sys.stdout, 'fasta')
+    p = argparse.ArgumentParser(description=__doc__)
+    # Arguments which may be generalizable to many scripts.
+    # TODO: Break these out into a module which can be imported.
+    p.add_argument("--in-fmt", "-f", dest='fmt_infile', nargs=1, type=str,
+                   metavar="FORMAT", default=DEFAULT_INFILE_FMT,
+                   help="file format of infile or stdin")
+    p.add_argument("--out-fmt", "-t", dest='fmt_outfile', nargs=1, type=str,
+                   metavar="FORMAT", default=DEFAULT_OUTFILE_FMT,
+                   help="file format of outfile or stdout")
+    p.add_argument("--verbose", "-v", action='count',
+                   help="increase verbosity")
+
+    # Arguments specific to this script.
+    p.add_argument('in_handles', nargs='*', type=argparse.FileType('r'),
+                   metavar="INFILE",
+                   default=[sys.stdin])
+
+    args = p.parse_args()
+
+    for handle in args.in_handles:
+        for trans_rec in translate_recs(parse(handle, args.fmt_infile)):
+            write(trans_rec, sys.stdout, args.fmt_outfile)
 
 if __name__ == '__main__':
     main()
--- ungap.py
+++ ungap.py
@@ -3,8 +3,45 @@
 
 from Bio.SeqIO import parse, write
 import sys
+import argparse
+from copy import copy
 
-for rec in parse(sys.stdin, 'fasta'):
-    rec.seq = rec.seq.ungap("-")
-    rec.seq = rec.seq.ungap(".")
-    write(rec, sys.stdout, 'fasta')
+def ungap_recs(records):
+    """Ungap sequence in records.
+
+    This generator takes an iterable of Bio.SeqRecord objects and produces
+    identical records, but with the sequences ungapped.
+
+    """
+    for rec in records:
+        rec = copy(rec)
+        rec.seq = rec.seq.ungap("-")
+        rec.seq = rec.seq.ungap(".")
+        yield rec
+
+
+def main():
+    p = argparse.ArgumentParser(description=__doc__)
+    # Arguments which may be generalizable to many scripts.
+    p.add_argument("--in-fmt", "-f", dest='fmt_infile', nargs=1, type=str,
+                   metavar="FORMAT", default='fasta',
+                   help="file format of infile or stdin")
+    p.add_argument("--out-fmt", "-t", dest='fmt_outfile', nargs=1, type=str,
+                   metavar="FORMAT", default='fasta',
+                   help="file format of outfile or stdout")
+    p.add_argument("--verbose", "-v", action='count',
+                   help="increase verbosity")
+
+    # Arguments specific to this script.
+    p.add_argument('in_handles', nargs='*', type=argparse.FileType('r'),
+                   metavar="INFILE",
+                   default=[sys.stdin])
+
+    args = p.parse_args()
+
+    for handle in args.in_handles:
+        for rec in ungap_recs(parse(handle, args.fmt_infile)):
+            write(trans_rec, sys.stdout, args.fmt_outfile)
+
+if __name__ == '__main__':
+    main()
