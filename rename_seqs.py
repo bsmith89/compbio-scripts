c04707ec1fd1f7e1257e68dea290c5d2d5f7ea45
Byron Smith <bsmith89@gmail.com>
2015-05-05 14:33:12 -0400

    Add renaming script.

---
+++ rename_seqs.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Given a mapping from IDs to new IDs and a sequence file, output the latter
+renamed.
+
+"""
+
+from Bio.SeqIO import parse, write
+import sys
+import argparse
+import cli
+import logging
+from copy import copy
+
+logger = logging.getLogger(__name__)
+
+def rename_recs(recs, name_map):
+    for rec in recs:
+        rec = copy(rec)
+        if rec.id in name_map:
+            rec.id = name_map[rec.id]
+        rec.name = ""
+        rec.description = ""
+        yield rec
+
+def get_map(handle):
+    out = {}
+    for line in handle:
+        key, value = line.split()
+        out[key.strip()] = value.strip()
+    return out
+
+def get_map_in_parser():
+    p = argparse.ArgumentParser(add_help=False)
+    g = p.add_argument_group(*POS_GROUP)
+    g.add_argument('map_handle',
+                   help="tab separated file mapping old to new IDs",
+                   metavar='MAP', type=argparse.FileType('r'))
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     parents=[cli.get_base_parser(),
+                                              cli.get_seq_out_parser(),
+                                              cli.get_map_in_parser(),
+                                              cli.get_seq_in_parser(),
+                                              ])
+    args = parser.parse_args(argv[1:])
+    return args
+
+def main():
+    args = parse_args(sys.argv)
+    logging.basicConfig(level=args.log_level)
+    logger.debug(args)
+
+    for rec in rename_recs(parse(args.in_handle, args.fmt_infile),
+                           get_map(args.map_handle)):
+        write(rec, args.out_handle, args.fmt_outfile)
+
+if __name__ == '__main__':
+    main()
