c71f11e3d487a4634f6012c4f47b304387ed5143
Byron Smith <bsmith89@gmail.com>
2015-05-14 17:52:53 -0400

    Revert to in-house codonalign

--- codonalign.py
+++ codonalign.py
@@ -1,23 +1,62 @@
 #!/usr/bin/env python3
 """Align codons based on a corresponding amino-acid sequence.
 
+Not defensive in the slightest:
+    Doesn't check that codons and AAs match.
+    Doesn't check that sequence labels match (see "match_order.py").
+
 """
 
+from Bio.SeqIO import parse, write
+from Bio.Seq import Seq
 import sys
 import argparse
+import lib.cli as cli
+from copy import copy
 import logging
 from warnings import warn
 
-from Bio.SeqIO import parse
-from Bio.AlignIO import read as read_aln
-from Bio.AlignIO import write
-from Bio.Alphabet import generic_protein, generic_nucleotide
-from Bio.codonalign import build as build_codon_alignment
-
-import lib.cli as cli
 
 logger = logging.getLogger(__name__)
-logging.captureWarnings(True)
+
+
+def codons(sequence):
+    """Return groups of 3 items from a sequence.
+    
+    This is specifically meant to return codons from an in-frame sequence."""
+    codon = ""
+    for nucl in sequence:
+        codon += nucl
+        if len(codon) == 3:
+            yield codon
+            codon = ""
+
+def backalign(nucl, prot):
+    align_nucl = ""
+    codon_iter = codons(nucl)
+    for amino in prot:
+        if amino == ".":
+            continue
+        elif amino.islower():
+            next(codon_iter)
+        elif amino == "-":
+            align_nucl += "---"
+        else:
+            align_nucl += next(codon_iter)
+    return Seq(align_nucl)
+
+def backalign_recs(nucl_recs, prot_index):
+    for nucl_rec in nucl_recs:
+        out_rec = copy(nucl_rec)
+        try:
+            prot_rec = prot_index[nucl_rec.id]
+        except KeyError:
+            logger.warn(("No alignment found for {}. "
+                         "Dropping.").format(nucl_rec.id))
+            continue
+        else:
+            out_rec.seq = backalign(nucl_rec.seq, prot_rec.seq)
+        yield out_rec
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(description=__doc__,
@@ -35,14 +74,12 @@ def main():
     logging.basicConfig(level=args.log_level)
     logger.debug(args)
 
-    prot_aln = read_aln(args.align_handle, args.fmt_align,
-                        alphabet=generic_protein)
-    nucl_recs = parse(args.in_handle, args.fmt_infile,
-                      alphabet=generic_nucleotide)
-    corr_dict = {rec.id: rec.id for rec in prot_aln}
+    align_index = {rec.id: rec
+                   for rec in parse(args.align_handle, args.fmt_align)}
 
-    codon_aln = build_codon_alignment(prot_aln, nucl_recs, corr_dict)
-    write(codon_aln, args.out_handle, args.fmt_outfile)
+    for rec in backalign_recs(parse(args.in_handle, args.fmt_infile),
+                              align_index):
+        write(rec, args.out_handle, args.fmt_outfile)
 
 if __name__ == '__main__':
     main()
