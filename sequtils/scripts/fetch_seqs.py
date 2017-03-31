0ff61c481cd5e65565a013c1cb1cae03fcf290b3
Byron Smith <me@byronjsmith.com>
2017-03-31 13:27:04 -0400

    Streamline fetch_seqs

--- sequtils/scripts/fetch_seqs.py
+++ sequtils/scripts/fetch_seqs.py
@@ -13,31 +13,28 @@ import logging
 logger = logging.getLogger(__name__)
 logging.captureWarnings(True)
 
-def get_recs(recs, get_ids):
-    for rec in recs:
-        if rec.id in get_ids:
+
+def fetch_iter(rec_iter, id_set):
+    for rec in rec_iter:
+        if id_set and rec.id in id_set:
+            id_set -= {rec.id}
             yield rec
-        else:
-            continue
 
-def get_rec_list(recs, get_ids):
-    map = {rec.id: rec for rec in recs}
-    return [map[id] for id in get_ids]
+def exclude_iter(rec_iter, id_set):
+    return (rec for rec in rec_iter if rec.id not in id_set)
 
-def get_list(handle):
-    out = []
-    for line in handle:
-        out.append(line.strip())
-    return out
+def order_iter(rec_iter, order):
+    index = {rec.id: rec for rec in rec_iter}
+    return (index[rec_id] for rec_id in order)
 
 def _get_extra_args():
     p = argparse.ArgumentParser(add_help=False)
     g = p.add_argument_group(*cli.FMT_GROUP)
     g.add_argument("-m", "--match-order", action='store_true',
                    help="output in the same order as LIST")
+    g.add_argument("-x", "--excluding", action='store_true',
+                   help="only output sequences *not* in LIST")
     return p
-
-
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(description=__doc__,
@@ -55,14 +52,22 @@ def main():
     logging.basicConfig(level=args.log_level)
     logger.debug(args)
 
-    if args.match_order:
-        for rec in get_recs(parse(args.in_handle, args.fmt_infile),
-                            get_list(args.list_handle)):
-            write(rec, args.out_handle, args.fmt_outfile)
+    if args.match_order and args.excluding:
+        raise ValueError("--match-order and --excluding cannot both be set.")
+
+    to_fetch = [line.strip() for line in args.list_handle]
+    fetch_set = set(to_fetch)
+
+    rec_iter = parse(args.in_handle, args.fmt_infile)
+
+    if args.excluding:
+        out_iter = exclude_iter(rec_iter, fetch_set)
+    elif args.match_order:
+        out_iter = order_iter(fetch_iter(rec_iter, fetch_set), to_fetch)
     else:
-        recs = get_rec_list(parse(args.in_handle, args.fmt_infile),
-                            get_list(args.list_handle))
-        write(recs, args.out_handle, args.fmt_outfile)
+        out_iter = fetch_iter(rec_iter, fetch_set)
+
+    write(out_iter, sys.stdout, args.fmt_outfile)
 
 if __name__ == '__main__':
     main()
